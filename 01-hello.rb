34d94088c3f1afef3a1347ef9488e16debb8a63a
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-07-09 06:31:13 +0800

    完成编程基础题目25

--- 01-hello.rb
+++ 01-hello.rb
@@ -1,8 +1,13 @@
 # 题目: 输入名字，输出 "Hello, 名字"
 
-print "请输入你的名字，然后按 Enter: "
-your_name = gets
+# print "请输入你的名字，然后按 Enter: "
+# your_name = gets
+#
+# # ...
+#
+# puts "你好，" + your_name
 
-# ...
-
-puts "你好，" + your_name
+arr =  ["a", "d", "d", "c", "b", "c", "c", "c", "d", "d", "e", "e", "e", "d", "a", "c", "e", "a", "d", "e"]
+a = arr.first
+a = arr - [a]
+puts a
